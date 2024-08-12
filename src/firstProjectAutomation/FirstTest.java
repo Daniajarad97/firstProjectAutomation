0d4bb3e74e0ea8cb81867db3536eda111127d65e
Dania Jarad <65604677+Daniajarad97@users.noreply.github.com>
2024-08-12 22:45:25 +0300

    update

--- src/firstProjectAutomation/FirstTest.java
+++ src/firstProjectAutomation/FirstTest.java
@@ -1,7 +1,5 @@
 package firstProjectAutomation;
 
-
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,33 +9,26 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class FirstTest {
-	
-	WebDriver driver = new ChromeDriver();
-	
-	//WebDriver driver = new EdgeDriver();
-	//WebDriver driver = new FirefoxDriver();
 
-	
-	
+	WebDriver driver = new ChromeDriver();
+
 	@BeforeTest
 	public void mySetup() {
-		
-		
+
 		driver.manage().window().maximize();
 		driver.get("https://www.google.co.uk/");
-		
+
 	}
-	
-	
+
 	@Test
 	public void firstTest() {
-		
+
 		driver.findElement(By.xpath("//*[@id=\"APjFqb\"]")).sendKeys("Dania Jarad LinkedIn");
-		driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]")).click();	
-		driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div[1]/div/div/span/a/h3")).click();	
-	   
-		//driver.quit();
-		//driver.close();
+		driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]")).click();
+		driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div[1]/div/div/span/a/h3")).click();
+
+		// driver.quit();
+		// driver.close();
 	}
 
 }
